dbaddb0affb613000915cf435a5d69c250045e72
Nitin Chaurasia <gs.nitin@gmail.com>
2019-07-18 02:09:44 -0400

    angular json updated & components added

---
+++ warehouse/src/app/inventory/inventory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nc-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.css']
+})
+export class InventoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ warehouse/src/app/shipping/ship-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nc-ship-order',
+  templateUrl: './ship-order.component.html',
+  styleUrls: ['./ship-order.component.css']
+})
+export class ShipOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
