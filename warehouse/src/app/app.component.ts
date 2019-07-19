92d3a9dc52ba236ef1d4f0ce1c2fd5b7ff675c88
Nitin Chaurasia <gs.nitin@gmail.com>
2019-07-19 00:02:37 -0400

    Lab 2 - adding sitewise styles

--- warehouse/src/app/app.component.ts
+++ warehouse/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Nitin Warehouse Management';
+  title = 'Nitin\'s Warehouse';
 }
--- warehouse/src/app/app.module.ts
+++ warehouse/src/app/app.module.ts
@@ -2,20 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { InventoryComponent } from './inventory/inventory.component';
-import { ShipOrderComponent } from './shipping/ship-order.component';
-import { ReceiveProductComponent } from './receiving/receive-product.component';
-import { OrdersToShipComponent } from './shipping/orders-to-ship.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    InventoryComponent,
-    ShipOrderComponent,
-    ReceiveProductComponent,
-    OrdersToShipComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
