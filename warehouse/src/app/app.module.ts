3c945f57cc26eb627e2c21ca27f8a0d884b1febf
Nitin Chaurasia <gs.nitin@gmail.com>
2019-07-19 00:12:16 -0400

    Scaffolding Components done

--- warehouse/src/app/app.module.ts
+++ warehouse/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ReceiveProductComponent } from './receiving/receive-product.component';
+import { OrdersToShipComponent } from './shipping/orders-to-ship.component';
+import { ShipOrderComponent } from './shipping/ship-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    InventoryComponent,
+    ReceiveProductComponent,
+    OrdersToShipComponent,
+    ShipOrderComponent
   ],
   imports: [
     BrowserModule
